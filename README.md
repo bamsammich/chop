b608e467afcdd67b2081c56eb7203ef3cbe51f3b
Travis Huddleston <travis.huddleston@gmail.com>
2022-11-23 12:07:04 -0500

    rev 1

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # chop
 CLI tool to make structured logging human-readable.
+
+## Usage
+
+```text
+Write structured log in a human-readable way.
+
+Usage:
+  chop [path] [flags]
+
+Flags:
+  -A, --after int                      print lines after this count
+  -B, --before int                     print lines before this count [-1 unsets this] (default -1)
+  -c, --columns strings                field names to extract to columns (default [message])
+  -x, --exclude-extra-fields columns   exclude extra fields not defined by columns
+  -h, --help                           help for chop
+      --max-column-width int           set maximum column width (default 60)
+  -m, --message-field string           field containing log message (default "message")
+```
+
+## How to use
+
+`chop` takes logs that look like this:
+
+```bash
+> cat example/app.log
+initializing app
+{"timestamp":"2022/01/01 01:00:00", "message": "App has started", "level": "INFO"}
+{"timestamp":"2022/01/01 01:00:01", "message": "Processing records", "level": "INFO"}
+{"timestamp":"2022/01/01 01:03:00", "message": "Error processing records: timeout", "level": "ERROR"}
+terminating app
+```
+
+And prints them like this:
+
+```bash
+> chop ./example/app.log
+   #  message                   fields
+   0  initializing app          map[]
+   1  App has started           map[level:INFO timestamp:2022/01/01 01:00:00]
+   2  Processing records        map[level:INFO timestamp:2022/01/01 01:00:01]
+   3  Error processing records  map[exception:timeout level:ERROR timestamp:2022/01/01 01:03:00]
+   4  terminating app           map[]
+```
+
+To add more/custom fields to `chop`'s output, pass additional fields as columns:
+
+```bash
+> chop ./example/app.log --columns timestamp,level,message
+   #  timestamp            level  message                   fields
+   0   ---                  ---   initializing app          map[]
+   1  2022/01/01 01:00:00  INFO   App has started           map[]
+   2  2022/01/01 01:00:01  INFO   Processing records        map[]
+   3  2022/01/01 01:03:00  ERROR  Error processing records  map[exception:timeout]
+   4   ---                  ---   terminating app           map[]
+```
+
+`chop` also accepts input from `stdin`:
+
+```bash
+> kubectl logs -n kube-system etcd-kind-control-plane | chop -m "msg" -c ts,level,caller,msg -B 5 -x
+   #  ts                        level  caller                msg
+   0  2022-11-23T16:34:44.841Z  info   etcdmain/etcd.go:73   Running:
+   1  2022-11-23T16:34:44.849Z  info   etcdmain/etcd.go:116  server has been already initialized
+   2  2022-11-23T16:34:44.849Z  info   embed/etcd.go:131     configuring peer listeners
+   3  2022-11-23T16:34:44.851Z  info   embed/etcd.go:479     starting with peer TLS
+   4  2022-11-23T16:34:44.858Z  info   embed/etcd.go:139     configuring client listeners
+   5  2022-11-23T16:34:44.862Z  info   embed/etcd.go:308     starting an etcd server
+```
---
+++ main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"text/tabwriter"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&messageField, "message-field", "m", defaultMessageField, "field containing log message")
+	rootCmd.PersistentFlags().StringSliceVarP(&columns, "columns", "c", []string{defaultMessageField}, "field names to extract to columns")
+	rootCmd.PersistentFlags().IntVar(&maxColumnWidth, "max-column-width", 60, "set maximum column width")
+	rootCmd.PersistentFlags().IntVarP(&beforeLine, "before", "B", -1, "print lines before this count [-1 unsets this]")
+	rootCmd.PersistentFlags().IntVarP(&afterLine, "after", "A", 0, "print lines after this count")
+	rootCmd.PersistentFlags().BoolVarP(&excludeExtraFields, "exclude-extra-fields", "x", false, "exclude extra fields not defined by `columns`")
+}
+
+var (
+	beforeLine int
+	afterLine  int
+
+	maxColumnWidth int
+	columns        []string
+
+	countHeader         = "   #"
+	fieldsHeader        = "fields"
+	defaultMessageField = "message"
+	messageField        string
+	excludeExtraFields  bool
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "chop [path]",
+	Short: "Write structured log in a human-readable way.",
+	Args:  cobra.RangeArgs(0, 1),
+	PreRun: func(cmd *cobra.Command, args []string) {
+		if messageField != defaultMessageField {
+			columns = replace(columns, defaultMessageField, messageField)
+		}
+	},
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		var (
+			w       = tabwriter.NewWriter(os.Stdout, 5, 1, 2, ' ', tabwriter.TabIndent)
+			headers = append([]string{countHeader}, columns...)
+		)
+		if !excludeExtraFields {
+			headers = append(headers, fieldsHeader)
+		}
+		printHeader(w, headers...)
+		if len(args) == 0 {
+			err = FromStdin(w)
+		} else {
+			err = FromFile(w, args[0])
+		}
+		w.Flush()
+		return
+	},
+}
+
+func FromStdin(w io.Writer) error {
+	stat, _ := os.Stdin.Stat()
+	if (stat.Mode() & os.ModeCharDevice) != 0 {
+		return fmt.Errorf("nothing passed to chop")
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	var count = 0
+	for scanner.Scan() {
+		if err := printLine(w, count, scanner.Text(), columns...); err != nil {
+			return err
+		}
+		count++
+	}
+
+	return nil
+}
+
+func FromFile(w io.Writer, path string) error {
+	fi, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	if fi.IsDir() {
+		return fmt.Errorf("must not be a directory")
+	}
+	file, err := os.Open(path)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+	scanner := bufio.NewScanner(file)
+	var count = 0
+	for scanner.Scan() {
+		if err := printLine(w, count, scanner.Text(), columns...); err != nil {
+			return err
+		}
+		count++
+	}
+	return nil
+}
+
+func printLine(w io.Writer, count int, line string, columns ...string) error {
+	if beforeLine >= 0 && count > beforeLine {
+		return nil
+	}
+	if count < afterLine {
+		return nil
+	}
+	fieldMap := make(map[string]any)
+	if !json.Valid([]byte(line)) {
+		line = fmt.Sprintf("{%q:%q}", messageField, line)
+	}
+	if err := json.Unmarshal([]byte(line), &fieldMap); err != nil {
+		return err
+	}
+	var message = fmt.Sprintf("%4d\t", count)
+	for _, col := range columns {
+		var (
+			val string
+			ok  bool
+		)
+		if val, ok = fieldMap[col].(string); !ok {
+			message = fmt.Sprintf("%s --- \t", message)
+			continue
+		}
+		if len(val) > maxColumnWidth && maxColumnWidth > 0 {
+			val = fmt.Sprintf("%s...", string(val[:maxColumnWidth]))
+		}
+		message = fmt.Sprintf("%s%s\t", message, val)
+		delete(fieldMap, col)
+	}
+	if !excludeExtraFields {
+		if len(fieldMap) > 0 {
+			message = fmt.Sprintf("%s%+v\t", message, fieldMap)
+		} else {
+			message = fmt.Sprintf("%s%+v\t", message, fieldMap)
+		}
+	}
+	fmt.Fprintln(w, message)
+	return nil
+}
+
+func printHeader(w io.Writer, columns ...string) {
+	var line string
+	for _, col := range columns {
+		if line == "" {
+			line = fmt.Sprintf("%s\t", col)
+		} else {
+			line = fmt.Sprintf("%s%s\t", line, col)
+		}
+	}
+	fmt.Fprintf(w, "%s\n", line)
+}
+
+func replace[T comparable](l []T, old, new T) []T {
+	for i, other := range l {
+		if other == old {
+			l[i] = new
+		}
+	}
+	return l
+}
+
+func main() {
+	rootCmd.Execute()
+}
